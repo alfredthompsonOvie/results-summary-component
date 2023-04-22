09220db137cd75c4ca6b264815cafd56d93a1b03
thompson <alfredthompsonovie@gmail.com>
2023-04-23 00:23:36 +0100

    fixed images not loading

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,12 +1,16 @@
 import data from "../data.json";
 
+function getImageUrl(name) {
+  return new URL(`../assets/images/${name}`, import.meta.url).href
+}
+
 export function fetchData() {
   const ul = document.querySelector(".summary__list");
 
   data.forEach(d => {
     const markup = `
     <li class="${d.category.toLowerCase()} result__item">
-								<img src="${d.icon}" alt="${d.category} icon" />
+								<img src="${getImageUrl(d.icon)}" alt="${d.category} icon" />
 								<span class="summary__result__title">${d.category}</span>
 								<span class="score__breakdown"
 									><span class="score__breakdown--actual"
